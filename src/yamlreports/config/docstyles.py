1919c4f412d0762bbb630c6d8e84ca2a4320c711
Connor Ferster <connorferster@gmail.com>
2026-05-30 23:46:48 -0700

    Working on style conversion to RL Style

--- src/yamlreports/config/docstyles.py
+++ src/yamlreports/config/docstyles.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel, Field, ConfigDict
+from typing import TypeAlias
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib import colors
 
 
-# --- Base Core Attributes ---
 class TextStyle(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
     
@@ -9,22 +11,58 @@ class TextStyle(BaseModel):
     size: int
     color: str
 
+    @property
+    def rlcolor(self) -> colors.Color:
+        """
+        Converts self.color into a ReportLab Color
+        """
+        # For named colors
+        if hasattr(colors, self.color):
+            return getattr(colors, self.color)
+        if isinstance(self.color, str):
+            return colors.HexColor(self.color.lstrip("#"))
+        # Fallback
+        return colors.Color(0, 0, 0)
 
-# --- Mixin Class ---
+
 class SpacingMixin:
-    spacing: int = Field(..., alias="spacing")
+    spacing: int = Field(alias="spacing")
 
 
-# --- Heading Model using Mixin ---
-class BodyTextStyle(SpacingMixin, TextStyle):
+class SymbolMixin:
+    symbol: str = Field(default="-")
+
+class BulletStyle(SpacingMixin, TextStyle):
     pass
+
+
+class BodyTextStyle(SpacingMixin, TextStyle):
+    bullets: BulletStyle
 
 class HeadingTextStyle(TextStyle):
     pass
 
-# --- Enclosing Document Style Model ---
 class ReportStyles(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
 
     body: BodyTextStyle
     headings: HeadingTextStyle
+
+    def to_rlstyle(self)  -> ParagraphStyle:
+        """
+        Returns a reportlab.lib.style.ParagraphStyle
+        """
+        leading = self.body.spacing * self.body.size
+        ParagraphStyle(
+            "name",
+            fontName=self.body.font,
+            fontSize=self.body.size,
+            leading=leading,
+            bulletFontSize=self.body.bullets.size,
+            textColor=self.body.rlcolor,
+
+
+        )
+
+
+StyleContainer: dict[str, ReportStyles]
