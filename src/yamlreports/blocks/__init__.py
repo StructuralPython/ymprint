0461851ee5667d87e2c7fc66de48ea674f43506e
Connor Ferster <connorferster@gmail.com>
2026-06-08 11:30:13 -0700

    Added blocks todo

---
+++ src/yamlreports/blocks/__init__.py
@@ -0,0 +1,5 @@
+from .admonition_block import check_admoninition_block, convert_admonition_block
+from .image_block import check_image_block, convert_image_block
+from .quote_block import check_quote_block, convert_quote_block
+
+# TODO: Create a block registration function and a singleton block registry
