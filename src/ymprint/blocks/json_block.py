051364a2a8da667e9c850b17be45621dce7e9ca4
Connor Ferster <connorferster@gmail.com>
2026-06-27 23:54:14 -0700

    Got jsonblock working; wrote document variables

--- src/ymprint/blocks/json_block.py
+++ src/ymprint/blocks/json_block.py
@@ -1,6 +1,6 @@
 from reportlab.platypus import HRFlowable
 from . import register_block
-from ..config.helpers import parse_width
+from ..config.helpers import parse_width, YMPrintValueError
 from typing import Any
 import pathlib
 
@@ -11,12 +11,19 @@ def convert_loadjson_block(block_key: str, block_value: dict, context: dict) -> 
     Loads a JSON file into the vars in the context
     """
     file_path = pathlib.Path(block_value['path'])
-    source_path = pathlib.Path(context['source_path'])
-    if not file_path.absolute:
-        file_path = source_path.parent / file_path
-    with open(file_path, 'r') as file:
+    source_path = pathlib.Path(context['source_path']).resolve()
+    json_path = source_path.parent / file_path
+    if not json_path.exists():
+        raise YMPrintValueError(
+            f"The loadjson block {block_key} file path does not exist: {json_path}"
+        )
+    with open(json_path, 'r') as file:
         data = json.load(file)
-    context['vars'][block_key] = data
+    namespace = block_value.get('namespace')
+    if namespace is not None:
+        context['vars'][namespace] = data
+    else:
+        context['vars'] = context['vars'].update(**data)
         
     return []
 
